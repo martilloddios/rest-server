7aa885b860849df2164e1001a486bbee22983bdb
JCEPA <javier.cepa.iglesias@gmail.com>
2020-11-21 20:28:31 +0100

    RestServer - usuario - jsonv3

--- server/config/config.js
+++ server/config/config.js
@@ -3,11 +3,11 @@ process.env.PORT = process.env.PORT || 3000;
 
 
 // ============ Entornos de BBDD
-process.env.MODE_ENV = process.env.MODE_ENV || 'dev';
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 let urlDB;
 
-if (process.env.MODE_ENV === 'dev') {
+if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
     urlDB = 'mongodb+srv://jcepa:8OrMSHwgr68fFCQg@cluster0.vahal.mongodb.net/cafe?retryWrites=true&w=majority';
